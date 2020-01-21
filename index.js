44597ff6be540b036053cc330f555276f7ce592d
Jack Reid <emailjackreid@gmail.com>
2020-01-21 21:30:27 +0000

    working download and unzip

--- index.js
+++ index.js
@@ -3,19 +3,53 @@ const puppeteer = require('puppeteer');
 const unzipper = require('unzipper');
 const auth = require('./auth');
 
-const DATA_URL = 'https://letterboxd.com/settings/data/';
+const SETTINGS_URL = 'https://letterboxd.com/settings/data';
 const DOWNLOAD_URL = 'https://letterboxd.com/data/export';
+const ARCHIVE_PATH = './output/archive.zip';
+const UNZIP_PATH = './output';
+
+async function downloadBlob(page) {
+  const data = await page.evaluate(async () => {
+    const resp = await window.fetch('https://letterboxd.com/data/export');
+
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+
+    const data = await resp.blob();
+    console.l;
+    const reader = new FileReader();
+    return new Promise(resolve => {
+      reader.addEventListener('loadend', () =>
+        resolve({
+          url: reader.result,
+          mime: resp.headers.get('Content-Type'),
+        })
+      );
+      reader.readAsDataURL(data);
+    });
+  });
+
+  return {
+    buffer: Buffer.from(data.url.split(',')[1], 'base64'),
+    mime: data.mime,
+  };
+}
+
+function unzip(archive, outputDir) {
+  return fs.createReadStream(archive).pipe(unzipper.Extract({path: outputDir}));
+}
 
 (async () => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(DATA_URL);
+    await page.goto(SETTINGS_URL);
 
     let pageTitle = await page.title();
 
     if (pageTitle.includes('Sign In')) {
-      console.log('Sign in required');
+      console.log('Signing in...');
     }
 
     const signInForm = await page.$('#signin-form');
@@ -33,22 +67,15 @@ const DOWNLOAD_URL = 'https://letterboxd.com/data/export';
 
     pageTitle = await page.title();
     if (pageTitle.includes('Update your settings')) {
-      console.log('Successfully logged in');
+      console.log('Signed in');
     }
 
-    const dlResponse = await page.evaluate(() => {
-      return fetch('https://letterboxd.com/data/export', {
-        method: 'GET',
-        credentials: 'include',
-      }).then(r => r.text());
-    });
+    console.log('Downloading archive');
+    const blob = await downloadBlob(page);
+    fs.writeFileSync(ARCHIVE_PATH, blob.buffer);
 
-    // https://github.com/ZJONSSON/node-unzipper
-    // https://github.com/puppeteer/puppeteer/issues/299
-    //fs.writeFileSync('./output/archive.zip', dlResponse);
-    //fs.createReadStream('./output/archive.zip').pipe(
-    //  unzipper.Extract({path: './output/'}),
-    //);
+    unzip(ARCHIVE_PATH, UNZIP_PATH);
+
     console.log('Done');
 
     await browser.close();
